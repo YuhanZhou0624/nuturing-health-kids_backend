e9baa41519d4994b548ba12a8c5de5abbab26f7f
Yuhan Zhou <yzho0250@student.monash.edu>
2025-05-01 03:17:16 +1000

    fix deployment error

--- app/db.py
+++ app/db.py
@@ -5,13 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DB_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 创建数据库引擎
 engine = create_engine(DATABASE_URL)
 
-# 创建 SessionLocal 类，用于依赖注入
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 声明基础类，用于继承定义 ORM 模型
 Base = declarative_base()
