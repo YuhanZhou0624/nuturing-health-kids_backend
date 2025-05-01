17b785da5b4c55b1d8cd6eaecbf87a4c4febe642
Yuhan Zhou <yzho0250@student.monash.edu>
2025-05-01 12:24:31 +1000

    test for azure database url

--- app/db.py
+++ app/db.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = (
+    "mssql+pyodbc://unionparty:monash101%21@ie-project.database.windows.net/iteration-1"
+    "?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=no"
+)
 
 engine = create_engine(DATABASE_URL)
 
